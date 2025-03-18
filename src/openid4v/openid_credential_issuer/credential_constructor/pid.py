fbdb3128ec4832e63a0b5cfa35a0dd37dcbc5353
Anna Alexeeva <anna@sunet.se>
2025-03-18 11:11:51 +0100

    Add PID and vctm data

--- src/openid4v/openid_credential_issuer/credential_constructor/pid.py
+++ src/openid4v/openid_credential_issuer/credential_constructor/pid.py
@@ -1,15 +1,133 @@
+import base64
 import json
 import logging
+from datetime import datetime, timedelta
 from typing import Optional, Union
 
 from idpyoidc.message import Message
 from idpysdjwt.issuer import Issuer
+from openid4v.openid_credential_issuer.credential import CredentialConstructor
 from satosa_idpyop.persistence import Persistence
 from satosa_idpyop.utils import combine_client_subject_id
 
-from openid4v.openid_credential_issuer.credential import CredentialConstructor
-
 logger = logging.getLogger(__name__)
+vctm_dict = {
+    "vct": "urn:eu.europa.ec.eudi:pid:1",
+    "name": "PID",
+    "description": "This is an PID document issued by the well know PID issuer",
+    "display": [
+        {
+            "lang": "en-US",
+            "name": "PID",
+            "rendering": {
+                "simple": {
+                    "logo": {
+                        "uri": "",
+                        "uri#integrity": "",
+                        "alt_text": "PID Card",
+                    },
+                    "background_color": "",
+                    "text_color": "",
+                },
+                "svg_templates": [
+                    {
+                        "uri": "https://raw.githubusercontent.com/AniaAlex/images/refs/heads/main/template-pid.svg",
+                        "uri#integrity": "",
+                        "properties": {
+                            "orientation": "",
+                            "color_scheme": "",
+                            "contrast": "",
+                        },
+                    }
+                ],
+            },
+        }
+    ],
+    "claims": [
+        {
+            "path": ["given_name"],
+            "display": [
+                {
+                    "lang": "en-US",
+                    "label": "Given Name",
+                    "description": "Given Name",
+                }
+            ],
+            "sd": "",
+            "svg_id": "given_name",
+        },
+        {
+            "path": ["family_name"],
+            "display": [
+                {
+                    "lang": "en-US",
+                    "label": "Last Name",
+                    "description": "Last Name",
+                }
+            ],
+            "sd": "",
+            "svg_id": "family_name",
+        },
+        {
+            "path": ["issuance_date"],
+            "display": [
+                {
+                    "lang": "en-US",
+                    "label": "Expiry Date",
+                    "description": "The date and time expired this pid",
+                }
+            ],
+            "sd": "",
+            "svg_id": "issuance_date",
+        },
+        {
+            "path": ["expiry_date"],
+            "display": [
+                {
+                    "lang": "en-US",
+                    "label": "Expiry Date",
+                    "description": "The date and time expired this pid",
+                }
+            ],
+            "sd": "",
+            "svg_id": "expiry_date",
+        },
+    ],
+    "schema_url": "",
+    "schema_url#integrity": "",
+    "extends": "",
+    "extends#integrity": "",
+}
+
+
+def calculate_age(born_date: datetime) -> int:
+    current_date = datetime.now()
+
+    age = current_date.year - born_date.year
+    if born_date.month > current_date.month:
+        age -= 1
+    elif born_date.month == current_date.month and born_date.day > current_date.day:
+        age -= 1
+
+    return age
+
+
+def convert_to_date(date_string: str) -> datetime:
+    try:
+        return datetime.strptime(date_string, "%Y-%m-%d")
+    except ValueError:
+        return "Invalid date format. Use YYYY-MM-DD."
+
+
+def is_over_18(date_string: str) -> bool:
+    born_date = convert_to_date(date_string)
+    return calculate_age(born_date) >= 18
+
+
+def get_vctm(vctm=vctm_dict):
+    json_string = json.dumps(vctm, separators=(",", ":"))
+    base64_encoded = base64.urlsafe_b64encode(json_string.encode()).decode()
+    return base64_encoded
 
 
 class PIDConstructor(CredentialConstructor):
@@ -69,6 +187,7 @@ class PIDConstructor(CredentialConstructor):
         logger.debug(f"Using {persistence.name} persistence layer")
         client_subject_id = combine_client_subject_id(client_id, user_id)
         authn_claims = persistence.load_claims(client_subject_id)
+        logger.debug(f"Anna {authn_claims}")
         # filter on accepted claims
         _ava = {}
         logger.debug(f"AVA claims: {authn_claims}")
@@ -84,16 +203,44 @@ class PIDConstructor(CredentialConstructor):
         if "identity" not in _body:
             _body["identity"] = {"schema": {"name": "DefaultSchema"}}
 
-        _body["identity"].update(_ava)
+        _body.update(_ava)
+
+        # TODO: age_over_18 is OPTIONAL:the plan to implement in May 2025
+        # TODO: FIX upon implementation there is a problmen in json
+        # meaning false values will be ommited
+        if "age_over_18" not in _body:
+            pass
+            # _body["age_over_18"]=is_over_18(_ava["birth_date"])
+        # MANDATORY: hardcoded
+        if "issuing_authority" not in _body:
+            _body["issuing_authority"] = "EU"
+        # MANDATORY: hardcoded
+        if "issuing_country" not in _body:
+            _body["issuing_country"] = "EU"
+        if "vct" not in _body:
+            _body["vct"] = "urn:eu.europa.ec.eudi:pid:1"
+        # MANDATORY: hardcoded
+        if "nationality" not in _body:
+            _body["nationality"] = ["EU"]
+        # MANDATORY: hardcoded
+        if "birth_place" not in _body:
+            _body["birth_place"] = "EU"
+
+        if "issuance_date" not in _body:
+            _body["issuance_date"] = datetime.today().strftime("%Y-%m-%d")
+        # MANDATORY: hardcoded : admin expiry, not technical
+        if "expiry_date" not in _body:
+            _body["expiry_date"] = (datetime.today() + timedelta(days=365)).strftime(
+                "%Y-%m-%d"
+            )
 
         # TODO: Need to be fixed in the future with the wallet public key.
         # TODO: See "Representation of an Asymmetric Proof-of-Possession Key".
         # The commented line below retrieves wallet public keys saved in `op_storage`.
         # The corresponding private keys were not found.
-        # The new flow works with Satosa's own key instead.
+        # The new flow works with Satosa's own keys instead.
         # _body["jwk"] = request["__verified_proof"].jws_header["jwk"]
-        if "vct" not in _body:
-            _body["vct"] = _body["document_type"]
+
         ci = Issuer(
             key_jar=self.upstream_get("attribute", "keyjar"),
             iss=self.upstream_get("attribute", "entity_id"),
@@ -105,10 +252,11 @@ class PIDConstructor(CredentialConstructor):
         )
 
         logger.debug(f"Combined body: {_body}")
-
+        vctm = get_vctm()
         _sdjwt = ci.create_holder_message(
-            payload=_body, jws_headers={"typ": "example+sd-jwt"}
+            payload=_body, jws_headers={"vctm": [vctm], "typ": "example+sd-jwt"}
         )
+
         return json.dumps({"credentials": [{"credential": _sdjwt}]})
 
 
